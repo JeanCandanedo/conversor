448410a650f6f1c089a524144f067e5e7bc6b3ce
Jean <jeandiaz24@hotmail.com>
2021-07-13 16:31:15 -0500

    message

---
+++ Notaciones/src/Packages/TreeTest.java
@@ -0,0 +1,109 @@
+package Packages;
+public class TreeTest {
+    class Nodo
+      {
+        int info;
+        Nodo izq, der;
+      }
+      Nodo raiz;
+
+      public TreeTest() {
+          raiz=null;
+      }
+      
+      public void insertar (int info)
+      {
+          Nodo nuevo;
+          nuevo = new Nodo ();
+          nuevo.info = info;
+          nuevo.izq = null;
+          nuevo.der = null;
+          if (raiz == null)
+              raiz = nuevo;
+          else
+          {
+              Nodo anterior = null, reco;
+              reco = raiz;
+              while (reco != null)
+              {
+                  anterior = reco;
+                  if (info < reco.info)
+                      reco = reco.izq;
+                  else
+                      reco = reco.der;
+              }
+              if (info < anterior.info)
+                  anterior.izq = nuevo;
+              else
+                  anterior.der = nuevo;
+          }
+      }
+
+
+      private void imprimirPre (Nodo reco)
+      {
+          if (reco != null)
+          {
+              System.out.print(reco.info + " ");
+              imprimirPre (reco.izq);
+              imprimirPre (reco.der);
+          }
+      }
+
+      public void imprimirPre ()
+      {
+          imprimirPre (raiz);
+          System.out.println();
+      }
+
+      private void imprimirEntre (Nodo reco)
+      {
+          if (reco != null)
+          {    
+              imprimirEntre (reco.izq);
+              System.out.print(reco.info + " ");
+              imprimirEntre (reco.der);
+          }
+      }
+
+      public void imprimirEntre ()
+      {
+          imprimirEntre (raiz);
+          System.out.println();
+      }
+
+
+      private void imprimirPost (Nodo reco)
+      {
+          if (reco != null)
+          {
+              imprimirPost (reco.izq);
+              imprimirPost (reco.der);
+              System.out.print(reco.info + " ");
+          }
+      }
+
+
+      public void imprimirPost ()
+      {
+          imprimirPost (raiz);
+          System.out.println();
+      }
+
+      public static void main (String [] ar)
+      {
+    	  TreeTest abo = new TreeTest ();
+    	  abo.insertar (400);
+          abo.insertar (100);
+          abo.insertar (200);
+          abo.insertar (700);
+          abo.insertar (50);
+          abo.insertar (75);
+          System.out.println ("Impresion preorden: ");
+          abo.imprimirPre ();
+          System.out.println ("Impresion entreorden: ");
+          abo.imprimirEntre ();
+          System.out.println ("Impresion postorden: ");
+          abo.imprimirPost ();        
+      }      
+}
